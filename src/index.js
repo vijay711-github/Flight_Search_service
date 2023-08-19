faaf9c59bac48ba33879d54d610cf751f24bdf65
vijay711-github <vj7112000@gmail.com>
2023-08-19 16:14:15 +0530

    added unique constarint to city model and created city repository

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
+const CityRepository = require("./repository/city-repository");
 
-const { PORT } = require('./config/serverConfig');
-const ApiRoutes = require('./routes/index');
-
-const db = require('./models/index');
+const db = require("./models/index");
 // const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
+	// create the express object
+	const app = express();
 
-    // create the express object
-    const app = express();
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+	app.use("/api", ApiRoutes);
 
-    app.use('/api', ApiRoutes);
-
-    app.listen(3000, async () => {
-        console.log(`Server started at ${PORT}`);
-        if(process.env.SYNC_DB) {
-            db.sequelize.sync({alter: true});
-        }
-    });
-}
+	app.listen(3000, async () => {
+		console.log(`Server started at ${PORT}`);
+		const repo = new CityRepository()
+		repo.createCity({name:"jalna"})
+		if (process.env.SYNC_DB) {
+			db.sequelize.sync({ alter: true });
+		}
+	});
+};
 
 setupAndStartServer();
